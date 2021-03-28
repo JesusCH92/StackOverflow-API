e402ce144496cb4fecc81cd620ef9b2b31cb6a32
JesusCH92 <jcocaharo@hotmail.com>
2021-03-28 23:44:57 +0200

    creating spy for check if getQuestionCollection method is called

---
+++ tests/Spy/QuestionRepositorySpy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Spy;
+
+use App\StackOverflow\Domain\Filter;
+use App\StackOverflow\Domain\QuestionRepository;
+
+class QuestionRepositorySpy implements QuestionRepository
+{
+    private $validateWasCalled = false;
+
+    public function getQuestionCollection(Filter $filter)
+    {
+        $this->validateWasCalled = true;
+    }
+
+    public function verify(): bool
+    {
+        return $this->validateWasCalled;
+    }
+}
