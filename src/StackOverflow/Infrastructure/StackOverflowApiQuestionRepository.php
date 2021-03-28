b02ed35e1e9070b3b1a87055e13f4e8772ce12af
JesusCH92 <jcocaharo@hotmail.com>
2021-03-29 00:03:29 +0200

    implementing Adapter for get question collection from stack overflow api

--- src/StackOverflow/Infrastructure/StackOverflowApiQuestionRepository.php
+++ src/StackOverflow/Infrastructure/StackOverflowApiQuestionRepository.php
@@ -6,11 +6,40 @@ namespace App\StackOverflow\Infrastructure;
 
 use App\StackOverflow\Domain\Filter;
 use App\StackOverflow\Domain\QuestionRepository;
+use App\StackOverflow\Infrastructure\Exception\StackOverflowApiServiceUnavailable;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class StackOverflowApiQuestionRepository implements QuestionRepository
 {
+    public const API = 'https://api.stackexchange.com/questions?order=desc&sort=activity&filter=default&site=stackoverflow';
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     public function getQuestionCollection(Filter $filter)
     {
+        $route = self::API;
 
+        $tagged = $filter->tagged()->taggedFilterFormat();
+        $fromDate = $filter->dateFilter()->fromDateFilterFormat();
+        $toDate = $filter->dateFilter()->toDateFilterFormat();
+
+        $response = $this->client->request(
+            'GET',
+            $route . $tagged . $fromDate . $toDate
+        );
+
+        $statusCode = $response->getStatusCode();
+
+        if (!in_array($statusCode, [200, 201])) {
+            throw new StackOverflowApiServiceUnavailable();
+        }
+
+        $content = $response->toArray();
+
+        return $content;
     }
 }
