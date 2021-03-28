9e2134306b1117c4dfc66eb6055d4e89530b9a2c
JesusCH92 <jcocaharo@hotmail.com>
2021-03-28 23:05:46 +0200

    updating controller, its not finished

--- src/Controller/StackOverflowApiController.php
+++ src/Controller/StackOverflowApiController.php
@@ -2,20 +2,41 @@
 
 namespace App\Controller;
 
+use App\StackOverflow\ApplicationService\DTO\QuestionGetterRequest;
+use App\StackOverflow\ApplicationService\QuestionGetter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StackOverflowApiController extends AbstractController
 {
+    private QuestionGetter $questionGetter;
+
+    public function __construct(QuestionGetter $questionGetter)
+    {
+        $this->questionGetter = $questionGetter;
+    }
+
     /**
      * @Route("/api", name="app_stack_overflow_api", methods={"GET"})
      */
-    public function index(): Response
+    public function index(Request $request): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/StackOverflowApiController.php',
-        ]);
+        $tagged = $request->query->get('tagged') === null ? '' : $request->query->get('tagged');
+        $fromDate = $request->query->get('from_date') === null ? '' : $request->query->get('from_date');
+        $toDate = $request->query->get('to_date') === null ? '' : $request->query->get('to_date');
+
+        $questionCollection = ($this->questionGetter)(
+            new QuestionGetterRequest(
+                $tagged,
+                $fromDate,
+                $toDate
+            )
+        );
+
+        return new JsonResponse(
+            $questionCollection
+        );
     }
 }
