d2f1c374e88a4e0d97b0746d60bb1c802ac8e9a0
JesusCH92 <jcocaharo@hotmail.com>
2021-03-28 22:31:57 +0200

    creating main controller for input API

---
+++ src/Controller/StackOverflowApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StackOverflowApiController extends AbstractController
+{
+    /**
+     * @Route("/api", name="app_stack_overflow_api", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/StackOverflowApiController.php',
+        ]);
+    }
+}
