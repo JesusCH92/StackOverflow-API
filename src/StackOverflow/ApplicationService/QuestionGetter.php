1e2cd3386bf92431e8b7ce2d91b1398fc7646b20
JesusCH92 <jcocaharo@hotmail.com>
2021-03-28 23:39:01 +0200

    updating service for get question collection

--- src/StackOverflow/ApplicationService/QuestionGetter.php
+++ src/StackOverflow/ApplicationService/QuestionGetter.php
@@ -5,11 +5,31 @@ declare(strict_types=1);
 namespace App\StackOverflow\ApplicationService;
 
 use App\StackOverflow\ApplicationService\DTO\QuestionGetterRequest;
+use App\StackOverflow\Domain\Filter;
+use App\StackOverflow\Domain\QuestionRepository;
+use App\StackOverflow\Domain\ValueObject\DateFilter;
+use App\StackOverflow\Domain\ValueObject\TaggedFilter;
 
 final class QuestionGetter
 {
+    private QuestionRepository $questionRepository;
+
+    public function __construct(QuestionRepository $questionRepository)
+    {
+        $this->questionRepository = $questionRepository;
+    }
+
     public function __invoke(QuestionGetterRequest $questionGetterRequest)
     {
-        return 'question collection';
+        $questionCollection = $this->questionRepository->getQuestionCollection(
+            new Filter(
+                new TaggedFilter($questionGetterRequest->tagged()),
+                new DateFilter(
+                    $questionGetterRequest->toDate(),
+                    $questionGetterRequest->fromDate()
+                )
+            )
+        );
+        return $questionCollection;
     }
 }
